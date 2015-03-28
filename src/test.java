0bb1d37681ec120a628a9892129fba989ffdb906
thinkpad-admin <editzhu@163.com>
2015-03-28 21:26:16 +0800

    d

--- src/test.java
+++ src/test.java
@@ -1,27 +1,14 @@
-import java.security.MessageDigest;
 
 public class test {
-    static char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-
-    public static void main(String[] args) {
-	try {
-	    MessageDigest md5 = MessageDigest.getInstance("MD5");// 申明使用MD5算法
-	    md5.update("1发发发发发发发发发发发发发发发发发发发发发发发发发发发发发发".getBytes());//
-	    System.out.println("md5(a)=" + byte2str(md5.digest()));
-
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+    String s;
+    {
+	s = new String("adf");
+	System.out.println(s);
     }
 
-    private static String byte2str(byte[] bytes) {
-	int len = bytes.length;
-	StringBuffer result = new StringBuffer();
-	for (int i = 0; i < len; i++) {
-	    byte byte0 = bytes[i];
-	    result.append(hex[byte0 >>> 4 & 0xf]);
-	    result.append(hex[byte0 & 0xf]);
-	}
-	return result.toString();
+    public static void main(String[] args) {
+	System.out.println("inside main()");
+	int[][] a1 = { { 1, 2, 3, }, { 4, 5, 6, }, };
+	System.out.println(a1[0].length);
     }
 }
