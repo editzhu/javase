9104b86392714d75d8a092f1026cf438f08f4ba7
editzhu@163.com <editzhu@163.com>
2016-08-11 13:19:02 +0800

    11

--- src/test.java
+++ src/test.java
@@ -1,14 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class test {
 
     public static void main(String[] args) {
-	Map<String, String> m = new HashMap<String, String>();
-	String s = "1";
-	for (int i = 0; i < 1000000000; i++) {
-	    m.put(s, s);
-	}
+	final int i = 1;
+	i = 0;
+	System.out.println(i);
 
     }
 }
